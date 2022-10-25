6fcbcdb6f77f1e60bba71350de148908c7ea7b1e
AkbarIlhamPerhanTI21A <114407901+AkbarIlhamPerhanTI21A@users.noreply.github.com>
2022-10-25 11:03:49 +0700

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,34 @@
+package encapsulation;
+
+/**
+ * Nama     : Akbar Ilham Perhan
+ * Kelas    : TI21A
+ * Sesi     : 5
+ */
+
+public class Person {
+   private String name, address;
+
+   public Person(String name, String address) {
+      this.name = name;
+      this.address = address;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+
+   public String getAddress() {
+      return address;
+   }
+
+   public void setAddress(String address) {
+      this.address = address;
+   }
+
+   @Override
+   public String toString() {
+      return name + "(" + address + ")";
+   }
+}
